80bb5d898b0437db75d775ad094312c655b51fbd
lirena <sakshamkushwaha12776@gmail.com>
2025-03-23 04:12:48 +0530

    .

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,3 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/prefer-optional-chain */
+
 "use client";
 import React, {
   createContext,
